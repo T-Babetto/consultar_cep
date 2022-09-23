d36ab91bd2634f4163ebbbb9ec3985854e4fce83
Tiago Rocha <tiagorocha.una@gmail.com>
2022-09-23 10:10:09 -0300

    feat: Enable user to provide the address to find the zip code

--- consultar_cep.py
+++ consultar_cep.py
@@ -1,20 +1,37 @@
 import requests
 
-cep = input('Digite o CEP ou logradouro:')
+menu = int(input('Para saber o endereço digite [1] e para saber o CEP digite [2]:'))
 
-if len(cep) == 8:
-    link = f'https://viacep.com.br/ws/{cep}/json/'
+if menu == 1:
+    cep = input('Digite o CEP:')
+    if len(cep) == 8:
+        link = f'https://viacep.com.br/ws/{cep}/json/'
 
-    resposta = requests.get(link)
+        resposta = requests.get(link)
 
-    dic_resposta = resposta.json()
+        dic_resposta = resposta.json()
 
-    uf = dic_resposta['uf']
-    cidade = dic_resposta['localidade']
-    bairro = dic_resposta['bairro']
-    localidade = dic_resposta['localidade']
-    logradouro = dic_resposta['logradouro']
-    cep = '{}-{}'.format(cep[:5], cep[5:])
-    print(f'{logradouro} - {bairro}, {localidade} - {uf}, {cep}')
-else:
-    print("CEP Inválido")
+        uf = dic_resposta['uf']
+        bairro = dic_resposta['bairro']
+        localidade = dic_resposta['localidade']
+        logradouro = dic_resposta['logradouro']
+        cep = '{}-{}'.format(cep[:5], cep[5:])
+        print(f'{logradouro} - {bairro}, {localidade} - {uf}, {cep}')
+    else:
+        print("CEP Inválido")
+elif menu == 2:
+    estado = str(input('Digite o UF:'))
+    municipio = str(input('Digite a cidade:'))
+    rua = str(input('Digite a rua:'))
+    if len(rua) > 3 and len(municipio) > 3:
+        link = f'https://viacep.com.br/ws/{estado}/{municipio}/{rua}/json/'
+
+        resposta = requests.get(link)
+
+        dic_resposta = list(resposta.json())
+
+        for dados in dic_resposta:
+            print(dados['logradouro'] + ' - ' + dados['bairro'] + ',' + dados['localidade'] + ' - ' + dados['uf'] + ', ' + dados['cep'])
+    else:
+        print("Endereço Inválido")
+
